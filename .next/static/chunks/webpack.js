6dbea3c0a27899c0d747094470961f1ace72aa6b
hakan.basturknewinn.com <forxynet@gmail.com>
2024-10-20 17:08:20 +0300

    added detail page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d2dcd26c47aec84d"; }
+/******/ 		__webpack_require__.h = function() { return "b2e531f4ebd772a5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
